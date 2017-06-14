907708fd7d5749a5ad782af56c416fe8f06fbf75
Kelly Murray <kmurray17@gmail.comm>
2017-06-14 10:18:34 -0400

    remove commented out junk

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -25,25 +25,6 @@ fetch('http://swapi.co/api/films')
         let characters = data.results[0].characters;
 
 
-        // console.log(characters == string);
-        //
-        // let id = characters.charAt(27);
-
-        //get people
-        //
-        // fetch('http://swapi.co/api/people/' + id)
-        //   .then(
-        //     function(response) {
-        //       if (response.status !== 200) {
-        //         console.log('Looks like there was a problem. Status Code: ' +
-        //           response.status);
-        //         return;
-        //       }
-        //         response.json().then(function(data) {
-        //           console.log(data.results[0].name);
-        //         })
-        //       }
-        //   )
 
         function renderCharacters(){
           return `
@@ -78,44 +59,3 @@ fetch('http://swapi.co/api/films')
       });
     }
   )
-
-
-
-
-
-
-
-// fetch('http://swapi.co/api/films', {
-//     mode: 'cors'
-// })
-//     .then(status)
-//     .then(json)
-//     .then(result)
-//     .then(function (films) {
-//     var filmsInfos = films.map(displayFilm)
-//         .reduce(function (a, b) {
-//         return a + b;
-//     }, '');
-//     document.getElementById('films').innerHTML = filmsInfos;
-//     return films;
-// });
-//
-// function displayFilm(film) {
-//     return '<div class="film">' + film.title + '</div>';
-// }
-//
-// function status(response) {
-//     if (response.status >= 200 && response.status < 300) {
-//         return Promise.resolve(response);
-//     } else {
-//         return Promise.reject(new Error(response.statusText));
-//     }
-// }
-//
-// function json(response) {
-//     return response.json();
-// }
-//
-// function result(json) {
-//     return json.results;
-// }
