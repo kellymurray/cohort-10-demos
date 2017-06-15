ba3a0b95407c99271426f946f08cbdb4c6043501
Kelly Murray <kmurray17@gmail.comm>
2017-06-15 11:11:30 -0400

    add jquery l33t sp3@k demo

---
+++ l33t-demo/main.js
@@ -0,0 +1,29 @@
+$(function () {
+  'use strict';
+
+  var leetChars = {
+    'a': '@',
+    'e': '3',
+    'i': '1',
+    'o': '0',
+    'u': '|_|'
+  };
+
+   function leet(str) {
+    return str.split('').map(function (characters) {
+      var replacement = leetChars[characters.toLowerCase()];
+      return replacement || characters;
+    }).join('');
+  }
+
+   function leetReg(str) {
+    return str.replace(/[aeiou]/g, function (message) {
+      return leetChars[message.toLowerCase()];
+    });
+  }
+
+   $('.leet-txt').on('input', function () {
+    $('.leet-preview').text(leetReg($(this).val()));
+  });
+
+});
